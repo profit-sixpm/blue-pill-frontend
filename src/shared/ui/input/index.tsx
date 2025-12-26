725d50e1b0abfc9d8cb97ff972a7ca3b61dae641
hov-i <dbsghdql55555@gmail.com>
2025-12-27 05:21:22 +0900

    styles: 검색창 크기 조절

--- src/shared/ui/input/index.tsx
+++ src/shared/ui/input/index.tsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "w-full shadow-[0px_0px_12px_0px_#1b1b1b14] border border-[#C9C9CB] rounded-[32px] py-4 px-6 outline-none focus:border-[#5978FF] transition-colors",
+          "w-full shadow-[0px_0px_14px_0px_#1b1b1b14] border border-[#C9C9CB] rounded-[36px] py-5 px-7 text-[18px] outline-none focus:border-[#5978FF] transition-colors",
           className
         )}
         ref={ref}
@@ -19,6 +19,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     );
   }
 );
+
 Input.displayName = "Input";
 
 export { Input };
